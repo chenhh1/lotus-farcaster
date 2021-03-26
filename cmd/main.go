c9c08f47edc6feba3ce7c3d74bfc89aee664b60c
chenhh <806176352@qq.com>
2021-03-26 15:36:20 +0800

    lotus_miner_sector_state

--- cmd/main.go
+++ cmd/main.go
@@ -15,6 +15,7 @@ import (
 	"math/big"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -67,7 +68,7 @@ func main() {
 
 	fmt.Println("# HELP lotus_chain_height return current height")
 	fmt.Println("# TYPE lotus_chain_height counter")
-	fmt.Print("lotus_chain_height { miner_id=",`"`, minerId,`"`, ", miner_host=",`"`, minerHost,`"`," } ", chainHead.Height(),"\n")
+	fmt.Print("lotus_chain_height { miner_id=", `"`, minerId, `"`, ", miner_host=", `"`, minerHost, `"`, " } ", chainHead.Height(), "\n")
 
 	// 生成钱包+锁定资金余额
 	// GENERATE WALLET + LOCKED FUNDS BALANCES
@@ -86,10 +87,10 @@ func main() {
 		}
 		addr := addr.String()
 		short := addr[0:5] + "..." + addr[len(addr)-5:]
-		//大整数     原值是:bigInt  -->  int  -->  bigFloat  -->   Float64
+		// 大整数     原值是:bigInt  -->  int  -->  bigFloat  -->   Float64
 		fBalance := new(big.Float).SetInt(balance.Int)
 		afterBalance, _ := fBalance.Float64()
-		fmt.Print("lotus_wallet_balance { miner_id=",`"`, minerId,`"`, ", miner_host=",`"`, minerHost,`"`,", address=",`"`, addr,`"`, ", short=",`"`,short,`"`, " } ", afterBalance/1000000000000000000.0,"\n")
+		fmt.Print("lotus_wallet_balance { miner_id=", `"`, minerId, `"`, ", miner_host=", `"`, minerHost, `"`, ", address=", `"`, addr, `"`, ", short=", `"`, short, `"`, " } ", afterBalance/1000000000000000000.0, "\n")
 
 	}
 
@@ -102,10 +103,10 @@ func main() {
 	}
 	fBalance := new(big.Float).SetInt(minerBalanceAvailable.Int)
 	afterBalance, _ := fBalance.Float64()
-	fmt.Print("lotus_wallet_balance { miner_id=",`"`,minerId,`"`, ", miner_host=",`"`, minerHost,`"`, ", address=",`"`, minerId,`"`, ", short=",`"`, minerId,`"`, " } ", afterBalance/1000000000000000000.0,"\n")
+	fmt.Print("lotus_wallet_balance { miner_id=", `"`, minerId, `"`, ", miner_host=", `"`, minerHost, `"`, ", address=", `"`, minerId, `"`, ", short=", `"`, minerId, `"`, " } ", afterBalance/1000000000000000000.0, "\n")
 
-	//生成矿工信息
-	//GENERATE MINER INFO
+	// 生成矿工信息
+	// GENERATE MINER INFO
 	minerVersion, err := storageMiner.Version(context.Background())
 	if err != nil {
 		fmt.Println("minerVersion error", err)
@@ -146,8 +147,8 @@ func main() {
 	fmt.Println("# TYPE lotus_miner_info gauge")
 	fmt.Println("# HELP lotus_miner_info_sector_size lotus miner sector size")
 	fmt.Println("# TYPE lotus_miner_info_sector_size gauge")
-	fmt.Print("lotus_miner_info { miner_id = ",`"`, minerId,`"`, ", miner_host = ",`"`, minerHost,`"`, ", version=",`"`, minerVersion.Version,`"`, ", owner=",`"`,minerOwner,`"`, ", owner_addr=",`"`, minerOwnerAddr,`"`,", worker=",`"`, minerWorker,`"`,", worker_addr=",`"`, minerWorkerAddr,`"`, ", control0=",`"`, minerControl0,`"`,", control0_addr=",`"`, minerControl0Addr,`"`," } 1","\n")
-	fmt.Print("lotus_miner_info_sector_size { miner_id = ",`"`, minerId,`"`, " } ", daemonStats.SectorSize,"\n")
+	fmt.Print("lotus_miner_info { miner_id = ", `"`, minerId, `"`, ", miner_host = ", `"`, minerHost, `"`, ", version=", `"`, minerVersion.Version, `"`, ", owner=", `"`, minerOwner, `"`, ", owner_addr=", `"`, minerOwnerAddr, `"`, ", worker=", `"`, minerWorker, `"`, ", worker_addr=", `"`, minerWorkerAddr, `"`, ", control0=", `"`, minerControl0, `"`, ", control0_addr=", `"`, minerControl0Addr, `"`, " } 1", "\n")
+	fmt.Print("lotus_miner_info_sector_size { miner_id = ", `"`, minerId, `"`, " } ", daemonStats.SectorSize, "\n")
 
 	// 生成daemon信息
 	// GENERATE DAEMON INFO
@@ -168,8 +169,65 @@ func main() {
 	}
 	fmt.Println("# HELP lotus_info lotus daemon information like adress version, value is set to network version number")
 	fmt.Println("# TYPE lotus_info gauge")
-	fmt.Print("lotus_info { miner_id=",`"`,minerId,`"`,", miner_host=",`"`, minerHost,`"`,", version=",`"`, daemonVersion.Version,`"`, ", network=",`"`,daemonNetwork,`"`, " } ", daemonNetworkVersion,"\n")
+	fmt.Print("lotus_info { miner_id=", `"`, minerId, `"`, ", miner_host=", `"`, minerHost, `"`, ", version=", `"`, daemonVersion.Version, `"`, ", network=", `"`, daemonNetwork, `"`, " } ", daemonNetworkVersion, "\n")
 
+	// 生成  SECTORS
+	// GENERATE SECTORS
+	fmt.Println("# HELP lotus_miner_sector_state sector state")
+	fmt.Println("# TYPE lotus_miner_sector_state gauge")
+	fmt.Println("# HELP lotus_miner_sector_event contains important event of the sector life")
+	fmt.Println("# TYPE lotus_miner_sector_event gauge")
+	fmt.Println("# HELP lotus_miner_sector_sealing_deals_info contains information related to deals that are not in Proving and Removed state.")
+	fmt.Println("# TYPE lotus_miner_sector_sealing_deals_info gauge")
+	sectorList, err := storageMiner.SectorsList(context.Background())
+	if err != nil {
+		fmt.Println("sectorList error", err)
+		return
+	}
+	for _, sector := range sectorList {
+		detail, err := storageMiner.SectorsStatus(context.Background(), sector, false)
+		if err != nil {
+			fmt.Println("sectorList error", err)
+			return
+		}
+		// 计算 0 出现在数组中的个数
+		a := 0
+		for _, j := range detail.Deals {
+			if j == 0 {
+				a++
+			}
+		}
+		deals := len(detail.Deals) - a
+		creationDate := detail.Log[0].Timestamp
+		packedDate := ""
+		finalizedDate := ""
+		verifiedWeight := detail.VerifiedDealWeight
+		for i := 0; i < len(detail.Log); i++ {
+			if detail.Log[i].Kind == "event;sealing.SectorPacked" {
+				packedDate = strconv.Itoa(int(detail.Log[i].Timestamp))
+			}
+			if detail.Log[i].Kind == "event;sealing.SectorFinalized" {
+				finalizedDate = strconv.Itoa(int(detail.Log[i].Timestamp))
+			}
+		}
+		var pledged int
+		if detail.Log[0].Kind == "event;sealing.SectorStartCC" {
+			pledged = 1
+		} else {
+			pledged = 0
+		}
+		fmt.Print("lotus_miner_sector_state { miner_id=", `"`, minerId, `"`, ", miner_host=", `"`, minerHost, `"`, ", sector_id=", `"`, sector, `"`, ", state=", `"`, detail.State, `"`, ", pledged=", `"`, pledged, `"`, ", deals=", `"`, deals, `"`, ", verified_weight=", `"`, verifiedWeight, `"`, " } 1\n")
+
+		if packedDate != "" {
+			fmt.Print("lotus_miner_sector_event { miner_id=", `"`, minerId, `"`, ", miner_host=", `"`, minerHost, `"`, ", sector_id=", `"`, sector, `", event_type="packed" } `, packedDate, "\n")
+		}
+		if string(creationDate) != "" {
+			fmt.Print("lotus_miner_sector_event { miner_id=", `"`, minerId, `"`, ", miner_host=", `"`, minerHost, `"`, ", sector_id=", `"`, sector, `", event_type="creation" } `, creationDate, "\n")
+		}
+		if finalizedDate != "" {
+			fmt.Print("lotus_miner_sector_event { miner_id=", `"`, minerId, `"`, ", miner_host=", `"`, minerHost, `"`, ", sector_id=", `"`, sector, `", event_type="finalized" } `, finalizedDate, "\n")
+		}
+	}
 }
 
 func apiURI(addr string) string {
@@ -227,8 +285,6 @@ func init() {
 	if err != nil {
 		fmt.Println("NewLotusStorageMiner error")
 	}
-
-
 
 }
 
